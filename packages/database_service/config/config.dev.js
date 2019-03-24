fb9afd9fbdbecfedee1af0fe2376c3b1bea97c23
Aurora Vaughn <avaughn.git@gmail.com>
2019-03-24 14:24:18 -0500

    secrets working in database service

--- packages/database_service/config/config.dev.js
+++ packages/database_service/config/config.dev.js
@@ -1,5 +1,7 @@
 const { PORT } = process.env;
+const { dbPassword } = require("../secrets");
+
 module.exports = {
   port: PORT || 4000,
-  mongoURI: `mongodb+srv://tofu:<password>@microservice-gufgp.mongodb.net/test?retryWrites=true`
+  mongoURI: `mongodb+srv://tofu:<${dbPassword}>@microservice-gufgp.mongodb.net/test?retryWrites=true`
 };
