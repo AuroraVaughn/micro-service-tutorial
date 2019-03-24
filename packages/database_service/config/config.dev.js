304f7af5271532969bce6e0e7be9064a72afcaca
Aurora Vaughn <avaughn.git@gmail.com>
2019-03-24 13:17:14 -0500

    .gitignore test commit

--- packages/database_service/config/config.dev.js
+++ packages/database_service/config/config.dev.js
@@ -1,4 +1,6 @@
 const { PORT } = process.env;
 module.exports = {
-  port: PORT || 4000
+  port: PORT || 4000,
+  mongoURI:
+    "mongodb+srv://tofu:<password>@microservice-gufgp.mongodb.net/test?retryWrites=true"
 };
